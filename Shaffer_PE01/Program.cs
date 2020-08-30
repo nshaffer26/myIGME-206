8462f598710afd2ad148961b27666728bbb3f135
nshaffer26 <nshaffer26@outlook.com>
2020-08-29 21:18:45 -0400

    Rename Shaffer_PE2 to Shaffer_PE02

---
+++ Shaffer_PE01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shaffer_PE1 {
+
+    // Class: Program
+    // Author: Nicholas Shaffer
+    // Date: 8/23/2020
+    // Purpose: Completion of Assignment PE1 by printing my name to the console
+    class Program {
+
+        // Method: Main
+        // Purpose: Print my name and age to the console
+        static void Main(string[] args) {
+
+            // String to hold my name
+            String name = "Nicholas Shaffer";
+
+            // Int to hold the result of a simple calculation to compute my age
+            int age = 2020 - 1998;
+
+            // Statements to print my name and age to the console
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"I am {age} years old");
+        }
+    }
+}
