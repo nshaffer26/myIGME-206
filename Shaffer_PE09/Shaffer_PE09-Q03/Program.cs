2c804511a22618e87e93efdf704197846e8b1b5e
nshaffer26 <nshaffer26@outlook.com>
2020-09-09 22:09:48 -0400

    Added Shaffer_PE09-Q03

---
+++ Shaffer_PE09/Shaffer_PE09-Q03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shaffer_PE09_Q03
+{
+    // Class: Program
+    // Author: Nicholas Shaffer
+    // Date: 9/9/2020
+    // Purpose: Use a delegate function to impersonate the Console.ReadLine() function when asking for user input
+    class Program
+    {
+        delegate string ReadLine();
+
+        // Method: Main
+        // Purpose: Define a variable of my delegate type and set it equal
+        static void Main(string[] args)
+        {
+            ReadLine line;
+            line = new ReadLine(Console.ReadLine);
+        }
+    }
+}
