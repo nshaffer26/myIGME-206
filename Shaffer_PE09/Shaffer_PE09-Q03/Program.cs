42251ed3a2f91ca8feba42baf39328c0af7dc0aa
nshaffer26 <nshaffer26@outlook.com>
2020-09-09 22:54:24 -0400

    Updated Shaffer_PE09-Q03

--- Shaffer_PE09/Shaffer_PE09-Q03/Program.cs
+++ Shaffer_PE09/Shaffer_PE09-Q03/Program.cs
@@ -8,14 +8,25 @@ namespace Shaffer_PE09_Q03
     // Purpose: Use a delegate function to impersonate the Console.ReadLine() function when asking for user input
     class Program
     {
-        delegate string ReadLine();
+        // Define the delegate method data type
+        delegate string DelegateReadLine();
 
         // Method: Main
         // Purpose: Define a variable of my delegate type and set it equal
         static void Main(string[] args)
         {
-            ReadLine line;
-            line = new ReadLine(Console.ReadLine);
+            // Declare a variable of type ReadLine 
+            DelegateReadLine line;
+
+            // Set the variable equal to the method I want to delegate, i.e., Console.ReadLine()
+            line = new DelegateReadLine(Console.ReadLine);
+
+            // Ask the user for a string
+            Console.WriteLine("Enter a string");
+            string input = line();
+
+            // Put that thing back where it came from
+            Console.WriteLine($"\n{input}\n");
         }
     }
 }
