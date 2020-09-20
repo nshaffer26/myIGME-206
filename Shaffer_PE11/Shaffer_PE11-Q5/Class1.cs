27b19ae49e758bf485fb0d81df71c2bb43684eea
nshaffer26 <nshaffer26@outlook.com>
2020-09-19 20:24:42 -0400

    Added Shaffer_PE11-Q5

---
+++ Shaffer_PE11/Shaffer_PE11-Q5/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Shaffer_PE11_Q5
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassengers()
+        {
+
+        }
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
+    public class Compact : Car, IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+    }
+    public class SUV : Car, IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+    }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+    }
+
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+        public void LoadPassenger()
+        {
+
+        }
+    }
+
+    public class FreightTrain : Train, IHeavyLoadCarrier
+    {
+
+    }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+}
