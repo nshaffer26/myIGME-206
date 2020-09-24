a836d43a01b2c1e54a6f1e35bce6939d49665b89
nshaffer26 <nshaffer26@outlook.com>
2020-09-23 20:57:20 -0400

    Added Shaffer_PE12-Q02

---
+++ Shaffer_PE12/Shaffer_PE12-Q02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Shaffer_PE12_Q02
+{
+    // Class: MyClass
+    // Author: Nicholas Shaffer
+    // Date: 9/23/2020
+    // Purpose: Parent class
+    public class MyClass
+    {
+        private string myString;
+
+		public string MyString
+        {
+            set
+            {
+                myString = value;
+            }
+        }
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+
+    }
+
+    // Class: MyDerivedClass
+    // Author: Nicholas Shaffer
+    // Date: 9/23/2020
+    // Purpose: Child class
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from the derived class)";
+        }
+
+        public static void Main(string[] args)
+        {
+            MyDerivedClass mDC1 = new MyDerivedClass();
+
+            mDC1.MyString = "Set string with property in parent class";
+            Console.WriteLine(mDC1.GetString() + "\n");
+        }
+    }
+}
