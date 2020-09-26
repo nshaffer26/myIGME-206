b05d08f2efed80771b69e14c1e743aceb097e88f
nshaffer26 <nshaffer26@outlook.com>
2020-09-26 18:16:24 -0400

    Shaffer_PE14

---
+++ Shaffer_PE14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaffer_PE14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass1 class1 = new MyClass1();
+            MyMethod(class1);
+
+            MyClass2 class2 = new MyClass2();
+            MyMethod(class2);
+        }
+
+        public static void MyMethod(object myObject)
+        {
+            IMyInterface myInterface = null;
+
+            if (myObject.GetType() == typeof(MyClass1))
+            {
+                myInterface = (MyClass1)myObject;
+            }
+            if(myObject.GetType() == typeof(MyClass2))
+            {
+                myInterface = (MyClass2)myObject;
+            }
+
+            myInterface.MyIMethod();
+        }
+    }
+
+    public interface IMyInterface
+    {
+        void MyIMethod();
+    }
+
+    public class MyClass1 : IMyInterface
+    {
+        public void MyIMethod()
+        {
+            Console.WriteLine("This is class 1");
+        }
+    }
+    public class MyClass2 : IMyInterface
+    {
+        public void MyIMethod()
+        {
+            Console.WriteLine("This is class 2");
+        }
+    }
+
+}
